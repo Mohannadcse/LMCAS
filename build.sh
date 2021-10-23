3a2486b6c3cc687fcc49e2e77d81d64c22ced7c1
Naman <naman.gupta@pm.me>
2021-10-22 23:09:59 -0500

    fix dockerfile

--- build.sh
+++ build.sh
@@ -1,29 +1,6 @@
 #!/bin/sh
 
 LLVM_VERSION=10
-
-# Install deps.
-echo "Build type set to: Release" && \
-    sudo apt-get update && \
-    sudo apt-get install -yqq software-properties-common && \
-    sudo apt-get update && \
-    sudo apt-get install -y wget libprotobuf-dev python-protobuf protobuf-compiler && \
-    sudo apt-get install -y python3-pip && \
-    sudo apt-get install -y libz3-dev && \
-    sudo apt-get install -y llvm-$LLVM_VERSION-dev && \
-    sudo apt-get install -y clang-$LLVM_VERSION && \
-    sudo apt-get install -y git && \
-    sudo apt-get install -y cmake && \
-    sudo apt-get install -y zlib1g-dev && \
-    sudo apt-get install -y build-essential &&\
-    sudo apt-get install -y gperf  libgoogle-perftools-dev && \
-    sudo apt-get install -yqq libboost-dev && \
-    sudo apt-get install -y flex && \
-    sudo apt-get install -y bison && \
-    sudo apt-get install nano && \
-    sudo apt-get install iputils-ping && \
-    sudo apt-get install -y libibverbs-dev
-
 export CC=clang-$LLVM_VERSION
 export CXX=clang++-$LLVM_VERSION
 export LLVM_CC_NAME clang-$LLVM_VERSION
@@ -33,7 +10,27 @@ export LLVM_AR_NAME llvm-ar-$LLVM_VERSION
 export LLVM_COMPILER clang-$LLVM_VERSION
 export FORCE_UNSAFE_CONFIGURE=1
 
-echo "Install WLLVM" && \
+echo "Installing dependencies." && \
+    sudo apt-get update && \
+    sudo apt-get install -y \
+        wget libprotobuf-dev python-protobuf protobuf-compiler  \
+        python3-pip  \
+        libz3-dev  \
+        llvm-$LLVM_VERSION-dev  \
+        clang-$LLVM_VERSION  \
+        git  \
+        cmake  \
+        zlib1g-dev  \
+        build-essential \
+        gperf  libgoogle-perftools-dev  \
+        libboost-dev  \
+        flex  \
+        bison  \
+        nano  \
+        iputils-ping \
+        libibverbs-dev
+
+echo "Installing WLLVM"  \
     pip3 install -U lit tabulate wllvm
 
 ROOTDIR=$(pwd)
@@ -41,14 +38,14 @@ ROOTDIR=$(pwd)
 # collect all binaries here
 mkdir $ROOTDIR/bin
 
-echo "Install klee-uclibc" && \
+echo "Installing klee-uclibc" && \
     git clone https://github.com/klee/klee-uclibc.git && \
     pushd klee-uclibc && \
     ./configure --make-llvm-lib --with-llvm-config=/usr/bin/llvm-config-$LLVM_VERSION && \
     make -j $(nproc) && \
     popd
 
-echo "Buildg KLEE" && \ 
+echo "Building KLEE" && \ 
     pushd partial-interpretation && \
     mkdir -p build && \
     cd build && \
@@ -63,7 +60,7 @@ echo "Buildg KLEE" && \
         make install && \
     popd
 
-echo "Build LLVM simplification passes" && \
+echo "Building LLVM simplification passes" && \
     pushd LLVM_Passes && \
     mkdir -p build && \
     cd build && \
